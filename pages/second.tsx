9ecdd4ea9e8faef1533d689700329317301f8698
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2022-03-20 02:27:42 +0900

    Add lib/hello.ts

--- pages/second.tsx
+++ pages/second.tsx
@@ -1,10 +1,12 @@
 import { NextPage } from "next";
 import Link from "next/link";
+import { hello } from "../lib/hello";
 
 const Second: NextPage = () => {
   return (
     <>
       <h1>Hello Next.js</h1>
+      <button onClick={() => hello()}>hello</button>
       <Link href="/" >index</Link>
     </>
   );
