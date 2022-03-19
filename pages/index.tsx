622c83af7c1f4b0089462cae231801da789f5ca9
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2022-03-20 02:18:20 +0900

    Add form

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,16 @@ import { useState } from "react";
 
 const Index: NextPage = () => {
   const [count, setCount] = useState<number>(0);
+  const [text, setText] = useState<string>("");
 
   return (
     <>
       <h1>Hello Next.js</h1>
       <h2>{count}</h2>
       <button onClick={() => setCount(count + 1)}>count</button>
+      <form>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </form>
     </>
   );
 };
